9471848b0c2b9085b17d2a79985ef57e730c8257
Dimitriscs1567 <tikasdimitrios@gmail.com>
2019-12-17 10:03:57 +0100

    added redux to burger example

--- src/BurgerExample/containers/Checkout/ContactData/ContactData.js
+++ src/BurgerExample/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
@@ -104,7 +105,7 @@ class ContactData extends Component {
         }
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price,
+            price: this.props.totalPrice,
             orderData: formData
         }
         axios.post( '/orders.json', order )
@@ -203,4 +204,11 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ingredients: state.ingredients,
+        totalPrice: state.totalPrice
+    };
+}
+
+export default connect(mapStateToProps)(ContactData);
